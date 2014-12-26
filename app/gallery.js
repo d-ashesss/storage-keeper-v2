2547b4e210a88936ba81fde8b986b9e03a6a3684
Sergey Mashentsev <d.ashesss@gmail.com>
2014-12-26 03:48:24 +0200

    tags

--- app/gallery.js
+++ app/gallery.js
@@ -16,3 +16,25 @@ exports.getImages = function(path, callback) {
 		callback(images);
 	});
 };
+
+exports.getTags = function(path, callback) {
+	var tags = node_path.basename(path).split(" ");
+	fs.readdir(path, function(err, files) {
+		var dirs = _.filter(files, function(file) {
+			var ext = node_path.extname(file).toLowerCase();
+			if (supported_extensions.indexOf(ext) >= 0) {
+				return false;
+			}
+			var file_path = path + "/" + file;
+			try {
+				var stat = fs.statSync(file_path);
+				return stat.isDirectory();
+			} catch(e) {}
+			return false;
+		});
+		tags = _.filter(tags.concat(dirs), function(tag) {
+			return /^[a-z0-9_\-]+$/i.test(tag);
+		});
+		callback(tags);
+	});
+};
--- gallery.js
+++ gallery.js
@@ -5,6 +5,7 @@ var node_path = require("path");
 
 var list = require("./app/list");
 var gallery = require("./app/gallery");
+var _ = require("underscore");
 
 var Image = require("./app/gallery/image");
 var Frame = require("./app/gallery/frame");
@@ -14,6 +15,13 @@ var Video = require("./app/gallery/video");
 var images_list;
 /** @type {List} */
 var history;
+
+//          Q   A   W   S   E   D   R   F   Z   X   C   V   1   2   3   4
+var keys = [81, 65, 87, 83, 69, 68, 82, 70, 90, 88, 67, 86, 49, 50, 51, 52];
+
+var tags_list = [];
+var sorted_images = [];
+var tag_counts = [];
 
 /** @type {Image} */
 var image;
@@ -31,12 +39,23 @@ $(function() {
 	video = new Video($("#current_video"), {
 		onSizeCallback: onObjectSize
 	});
+	$("#new_tag_form").submit(function(event) {
+		event.preventDefault();
+		var new_tag = this["tag_name"].value.replace(/[^0-9a-z_\-]/ig, '');
+		if (new_tag.length > 0 && tags_list.indexOf(new_tag) < 0) {
+			tags_list.push(new_tag);
+			drawKeymap();
+		}
+		this["tag_name"].value = "";
+		this["tag_name"].blur();
+	});
 
 	var dir = sessionStorage.getItem(OPEN_DIR);
 	if (!dir) {
 		window.location = "index.html";
 		return;
 	}
+	$("#new_tag_panel").hide();
 
 	$(window)
 		.bind("mousewheel", function(/** @type {jQuery.Event} */ event) {
@@ -47,9 +66,26 @@ $(function() {
 			}
 		})
 		.keydown(function(/** @type {jQuery.Event} */ event) {
+			if ($(event.target).is("input")) {
+				return;
+			}
+			var current_image_index = images_list.getPosition();
+			var current_tag_index = sorted_images[current_image_index];
+			var key_index = keys.indexOf(event.keyCode);
 			if (event.shiftKey && event.keyCode == 32 /* space */) {
 				show(SHOW.PREV);
 			} else if (event.shiftKey && event.keyCode == 46 /* delete */) {
+				if (typeof current_tag_index != "undefined") {
+					tag_counts[current_tag_index]--;
+				}
+				if (tags_list.indexOf("delete") < 0) {
+					tags_list.push("delete");
+				}
+				current_tag_index = sorted_images[current_image_index] = tags_list.indexOf("delete");
+				tag_counts[current_tag_index]++;
+				drawKeymap();
+			} else if (event.shiftKey && event.keyCode == 187 /* equal */) {
+				$("#new_tag_form").find("input").focus();
 			} else if (event.keyCode == 13 /* enter */) {
 			} else if (event.shiftKey || event.ctrlKey || event.altKey) {
 				return;
@@ -75,9 +111,34 @@ $(function() {
 			} else if (event.keyCode == 192 /* tilda */) {
 				$(".overlay").fadeToggle("fast");
 			} else if (event.keyCode == 45 /* insert */) {
+				if (typeof current_tag_index != "undefined") {
+					tag_counts[current_tag_index]--;
+				}
+				if (typeof current_tag_index == "undefined" || current_tag_index == (tags_list.length - 1)) {
+					current_tag_index = sorted_images[current_image_index] = 0;
+				} else {
+					current_tag_index = ++sorted_images[current_image_index];
+				}
+				tag_counts[current_tag_index]++;
+				drawKeymap();
 			} else if (event.keyCode == 46 /* delete */) {
+				tag_counts[current_tag_index]--;
+				delete sorted_images[current_image_index];
+				drawKeymap();
+			} else if (key_index >= 0 && typeof tags_list[key_index] != "undefined") {
+				if (current_tag_index != key_index) {
+					if (typeof current_tag_index != "undefined") {
+						tag_counts[current_tag_index]--;
+					}
+					tag_counts[key_index]++;
+					sorted_images[current_image_index] = key_index;
+				} else {
+					tag_counts[key_index]--;
+					delete sorted_images[current_image_index];
+				}
+				drawKeymap();
 			} else {
-				console.log(event.keyCode);
+				console.info(event.keyCode, String.fromCharCode(event.keyCode));
 				return;
 			}
 			event.preventDefault();
@@ -94,6 +155,7 @@ $(function() {
 			unique: true
 		});
 
+		loadTags(dir);
 		show();
 	});
 });
@@ -109,6 +171,47 @@ function onObjectSize(naturalWidth, naturalHeight, actualWidth, actualHeight) {
 	var actual = actualWidth * actualHeight;
 	var scale = actual / natural * 100;
 	$("#current_file_size").text(naturalWidth + '×' + naturalHeight + ' ' + scale.toFixed(0) + '%');
+}
+
+function loadTags(dir) {
+	sorted_images = [];
+	tag_counts = _.map(keys, function() {return 0});
+	gallery.getTags(dir, function(tags) {
+		tags_list = tags;
+		drawKeymap();
+	});
+}
+
+function drawKeymap() {
+	var $keymap = $('#keymap').empty();
+
+	var selected_count = 0;
+	_.each(keys, function(key_code, key_index) {
+		if (typeof tags_list[key_index] == "undefined") {
+			return;
+		}
+		var count = "";
+		if (tag_counts[key_index] > 0) {
+			selected_count += tag_counts[key_index];
+			count = " (" + tag_counts[key_index] + ")";
+		}
+		var text = /*String.fromCharCode(key_code) + ": " + */tags_list[key_index] + count;
+		var $tag = $("<div>", { class: "tag" }).text(text).appendTo($keymap);
+		$("<span>", { class: "key" }).text(String.fromCharCode(key_code)).prependTo($tag);
+
+		var image_index = images_list.getPosition();
+		if (sorted_images[image_index] == key_index) {
+			$tag.addClass("current");
+		}
+	});
+
+	$("<div>").html("&nbsp;").appendTo($keymap);
+	$("<div>")
+		.text("selected: " + selected_count)
+		.appendTo($keymap);
+	$("<div>")
+		.text("skipped: " + (sorted_images.length - selected_count))
+		.appendTo($keymap);
 }
 
 /**
@@ -191,4 +294,5 @@ function show(direction) {
 	if (next_image != null) {
 		image_preload.attr("src", next_image);
 	}
+	drawKeymap();
 }
